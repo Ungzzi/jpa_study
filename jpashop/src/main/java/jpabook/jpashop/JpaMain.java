edf37ac3368e27ab358bd5773fa0196fa18c0288
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-08-21 17:17:54 +0900

    jpa - 실전 예제 2 : 연관관계 매핑(양방향/단방향)

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -15,6 +18,19 @@ public class JpaMain {
         tx.begin();
 
         try {
+            /** 1.양방향
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
+             */
+
+            /* 2. 단방향 */
+            Order order = new Order();
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
