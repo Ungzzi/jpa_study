85465a02f22a6899bf4a3bb573ae8922941ee775
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-08-23 14:58:39 +0900

    jpa - mappedSuperClass (매핑 정보 상속) 학습

--- ex01_hello_jpa/src/main/java/JpaBasic/JpaMain.java
+++ ex01_hello_jpa/src/main/java/JpaBasic/JpaMain.java
@@ -1,9 +1,12 @@
 package JpaBasic;
 
+import net.bytebuddy.asm.Advice;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -17,19 +20,15 @@ public class JpaMain {
 
         try {
 
-            Movie movie = new Movie();
-            movie.setDirector("Nolan");
-            movie.setActor("Mat");
-            movie.setName("Interstellar");
-            movie.setPrice(15000);
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreatedBy("J");
+            member.setCreatedDate(LocalDateTime.now());
 
-            em.persist(movie);
-            
+            em.persist(member);
+
             em.flush();
             em.clear();
-
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         } catch (Exception e) {
